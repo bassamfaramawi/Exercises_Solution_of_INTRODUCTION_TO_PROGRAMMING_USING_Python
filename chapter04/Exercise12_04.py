16c4d1a82ea1463acde1ea036830ec91ad3eee83
Bassam Faramawi <tiodaronzi3@yahoo.com>
2018-09-17 22:14:29 +0300

    Exercises solution of chapter 04

--- chapter04/Exercise12_04.py
+++ chapter04/Exercise12_04.py
@@ -13,7 +13,7 @@ one of them (but not both).
 number = eval(input("Enter an integer: "))
 
 # Check the cases and display the result
-print("Is 10 divisible by 5 and 6?", number % 5 == 0 and number % 6 == 0)
-print("Is 10 divisible by 5 or 6?", number % 5 == 0 or number % 6 == 0)
-print("Is 10 divisible by 5 and 6, but not both?", (number % 5 == 0 or \
-                                                    number % 6 == 0) and not (number % 5 == 0 and number % 6 == 0))
+print("Is", str(number), "divisible by 5 and 6?", number % 5 == 0 and number % 6 == 0)
+print("Is", str(number), "divisible by 5 or 6?", number % 5 == 0 or number % 6 == 0)
+print("Is", str(number), "divisible by 5 and 6, but not both?", (number % 5 == 0 or \
+                                                                 number % 6 == 0) and not (number % 5 == 0 and number % 6 == 0))
